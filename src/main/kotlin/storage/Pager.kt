df092a2ac6f2e18220df8ae3796a849178d380d6
Thomas Basche <thomas.basche@wolt.com>
2023-06-11 14:31:17 +0300

    Track the current page

--- src/main/kotlin/storage/Pager.kt
+++ src/main/kotlin/storage/Pager.kt
@@ -5,11 +5,15 @@ import java.io.File
 data class Pager(
     val file: File,
     var pages: MutableList<Page>,
+    val currentPageNumber: Int,
 ) {
     companion object {
         fun open(dbName: String) = Pager(
             file = File(dbName),
             pages = mutableListOf(),
+            currentPageNumber = 0
         )
     }
 }
+
+fun Pager.currentPage(): Page = this.pages[currentPageNumber]
--- src/main/kotlin/storage/rowStorage.kt
+++ src/main/kotlin/storage/rowStorage.kt
@@ -2,19 +2,19 @@ package storage
 
 import statement.Row
 
-// TODO this needs some love
 fun allocateRow(pager: Pager, collectionName: String, row: Row) {
     if (pager.pages.isEmpty())
         pager.pages = mutableListOf(Page(rows = mutableMapOf()))
 
-    pager.pages.first().rows[collectionName] = pager.pages.first().rows[collectionName] ?: mutableListOf()
-    pager.pages.first().rows[collectionName]?.add(serializeRow(row))
+    // TODO determine if another page is required (ie. it's reached max rows)
+    // TODO this should then 'know' the current page to be allocated without having to iterate the whole lot every time
+    pager.currentPage().rows[collectionName] = pager.currentPage().rows[collectionName] ?: mutableListOf()
+    pager.currentPage().rows[collectionName]?.add(serializeRow(row))
 }
 
 fun retrieveRows(pager: Pager, collectionName: String): List<Row> {
-    val pages = pager.pages
-    if (pages.isEmpty()) return emptyList()
-    val rows = pages.first().rows[collectionName] ?: return emptyList()
+    if (pager.pages.isEmpty()) return emptyList()
+    val rows = pager.currentPage().rows[collectionName] ?: return emptyList()
     return rows.map(::deserializeRow)
 }
 
