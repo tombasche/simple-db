fc4cbc779cc032fbfbe45f9ec91e37caf8d606c7
Thomas Basche <thomas.basche@wolt.com>
2023-06-11 14:52:16 +0300

    Refactor a tad

--- src/main/kotlin/storage/rowStorage.kt
+++ src/main/kotlin/storage/rowStorage.kt
@@ -6,12 +6,12 @@ fun allocateRow(pager: Pager, collectionName: String, row: Row) {
     if (pager.reachedMax()) {
         pager.newPage()
     }
-    pager.currentPage().rows[collectionName] = pager.currentPage().rows[collectionName] ?: mutableListOf()
-    pager.currentPage().rows[collectionName]?.add(serializeRow(row))
+    val currentPage = pager.currentPage()
+    currentPage.rows[collectionName] = currentPage.rows[collectionName] ?: mutableListOf()
+    currentPage.rows[collectionName]?.add(serializeRow(row))
 }
 
 fun retrieveRows(pager: Pager, collectionName: String): List<Row> {
-    if (pager.pages.isEmpty()) return emptyList()
     val rows = pager.currentPage().rows[collectionName] ?: return emptyList()
     return rows.map(::deserializeRow)
 }
